d0679c0471a0c153d15f00bf559795d49b2a7757
Sadik Besata <sadik.bsata89@gmail.com>
2023-04-12 16:07:22 +0300

    Organize Towers folder

--- Assets/Towers/Bullet/Bullet.cs
+++ Assets/Towers/Bullet/Bullet.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
@@ -45,7 +43,6 @@ public class Bullet : MonoBehaviour
     {
         //GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
         //Destroy(effectIns, 2f);
-
         if (explosionRadius > 0f)
         {
             Explode();
